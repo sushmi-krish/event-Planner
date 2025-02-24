64c86696034b126a75008cfe5fd522c908037884
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-02-24 11:36:41 -0500

    deployement

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:"event-Planner"
-  plugins: [react()],
+   base:"event-Planner" ,  
+   plugins: [react()],
 })
